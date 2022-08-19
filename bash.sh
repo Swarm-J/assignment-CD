f2b699bdf001d24c8435dd80ac288cb06942e101
Swarm-J <julianschinkel@gmail.com>
2022-08-19 17:30:58 +0200

    changed back to an earlier build

--- bash.sh
+++ bash.sh
@@ -2,8 +2,6 @@ echo "Moving to directory"
 cd  /home/farm
 echo "Listing contents"
 pwd
-echo "Grabbing files"
-git pull
 echo "Restarting farm process"
 systemctl restart farm
 echo "Successfully restarted process"
