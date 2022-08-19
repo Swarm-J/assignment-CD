c25285ce23a23a10641a625a476a15455d57a330
Swarm-J <julianschinkel@gmail.com>
2022-08-19 17:12:31 +0200

    another one

--- bash.sh
+++ bash.sh
@@ -2,6 +2,8 @@ echo "Moving to directory"
 cd  /home/farm
 echo "Listing contents"
 pwd
+echo "Grabbing files"
+git pull
 echo "Restarting farm process"
 systemctl restart farm
 echo "Successfully restarted process"
